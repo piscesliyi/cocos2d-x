a04e028974fbf565e84b0e2841717d5cea93b78f
piscesliyi <piscesliyi@qq.com>
2013-09-16 16:49:02 +0800

    Main menu data optimize

--- PlayfulChick/Classes/Constant.h
+++ PlayfulChick/Classes/Constant.h
@@ -12,4 +12,13 @@
 #define DESIGN_WIDTH    960
 #define DESIGN_HEIGHT   540
 
+#define DATA_NAME_SOUNDON   "soundon"
+
+#define POS_STARTBTN_X  117
+#define POS_STARTBTN_Y  312
+#define POS_SOUNDBTN_X  790
+#define POS_SOUNDBTN_Y  110
+#define POS_EXITBTN_X   870
+#define POS_EXITBTN_Y   110
+
 #endif
--- PlayfulChick/Classes/MainmenuScene.cpp
+++ PlayfulChick/Classes/MainmenuScene.cpp
@@ -8,6 +8,7 @@
 
 #include "MainmenuScene.h"
 
+#include "Constant.h"
 #include "Tools.h"
 #include "VisibleRect.h"
 #include "SimpleAudioEngine.h"
@@ -30,7 +31,7 @@ bool MainmenuScene::init()
         return false;
     }
     
-    Tools::m_bSoundOn = CCUserDefault::sharedUserDefault()->getBoolForKey("soundon", true);
+    Tools::m_bSoundOn = CCUserDefault::sharedUserDefault()->getBoolForKey(DATA_NAME_SOUNDON, true);
     
     //background
     CCSprite* menuBg = CCSprite::create("menu_bg.png");
@@ -38,26 +39,26 @@ bool MainmenuScene::init()
     this->addChild(menuBg);
     
     //start button
-    CCSprite* startBtnImage = CCSprite::create("menu_start.png");
-    CCRect normalRect = CCRectMake(0, 0, startBtnImage->getContentSize().width/2, startBtnImage->getContentSize().height);
-    CCRect selectedRect = CCRectMake(startBtnImage->getContentSize().width/2, 0, startBtnImage->getContentSize().width/2, startBtnImage->getContentSize().height);
+    CCSize imageSize = CCSprite::create("menu_start.png")->getContentSize();
+    CCRect normalRect = CCRectMake(0, 0, imageSize.width/2, imageSize.height);
+    CCRect selectedRect = CCRectMake(imageSize.width/2, 0, imageSize.width/2, imageSize.height);
     CCSprite* startBtnNormal = CCSprite::create("menu_start.png", normalRect);
     CCSprite* startBtnSelected = CCSprite::create("menu_start.png", selectedRect);
     CCMenuItemSprite* startBtn = CCMenuItemSprite::create(startBtnNormal, startBtnSelected, this, menu_selector(MainmenuScene::clickStartBtn));
     startBtn->setAnchorPoint(ccp(0, 1));
-    startBtn->setPosition(Tools::getFinalPosition(ccp(117, 312)));
+    startBtn->setPosition(Tools::getFinalPosition(ccp(POS_STARTBTN_X, POS_STARTBTN_Y)));
     
     //sound button
-    CCSprite* soundBtnImage = CCSprite::create("menu_sound.png");
-    normalRect = CCRectMake(0, 0, soundBtnImage->getContentSize().width/2, soundBtnImage->getContentSize().height);
-    selectedRect = CCRectMake(soundBtnImage->getContentSize().width/2, 0, soundBtnImage->getContentSize().width/2, soundBtnImage->getContentSize().height);
+    imageSize = CCSprite::create("menu_sound.png")->getContentSize();
+    normalRect = CCRectMake(0, 0, imageSize.width/2, imageSize.height);
+    selectedRect = CCRectMake(imageSize.width/2, 0, imageSize.width/2, imageSize.height);
     CCSprite* soundBtnNormal = CCSprite::create("menu_sound.png", normalRect);
     CCSprite* soundBtnSelected = CCSprite::create("menu_sound.png", selectedRect);
-    CCMenuItemSprite* itemSoundon = CCMenuItemSprite::create(soundBtnNormal, NULL, this, NULL);
-    CCMenuItemSprite* itemSoundoff = CCMenuItemSprite::create(soundBtnSelected, NULL, this, NULL);
+    CCMenuItemSprite* itemSoundon = CCMenuItemSprite::create(soundBtnNormal, NULL);
+    CCMenuItemSprite* itemSoundoff = CCMenuItemSprite::create(soundBtnSelected, NULL);
     CCMenuItemToggle* soundBtn = CCMenuItemToggle::createWithTarget(this, menu_selector(MainmenuScene::clickSoundBtn), itemSoundon, itemSoundoff, NULL);
     soundBtn->setAnchorPoint(ccp(0, 1));
-    soundBtn->setPosition(Tools::getFinalPosition(ccp(790, 110)));
+    soundBtn->setPosition(Tools::getFinalPosition(ccp(POS_SOUNDBTN_X, POS_SOUNDBTN_Y)));
     if (Tools::m_bSoundOn)
     {
         CocosDenshion::SimpleAudioEngine::sharedEngine()->playBackgroundMusic("background.mp3", true);
@@ -68,15 +69,16 @@ bool MainmenuScene::init()
     }
     
     //exit button
-    CCSprite* exitBtnImage = CCSprite::create("menu_exit.png");
-    normalRect = CCRectMake(0, 0, exitBtnImage->getContentSize().width/2, exitBtnImage->getContentSize().height);
-    selectedRect = CCRectMake(exitBtnImage->getContentSize().width/2, 0, exitBtnImage->getContentSize().width/2, exitBtnImage->getContentSize().height);
+    imageSize = CCSprite::create("menu_exit.png")->getContentSize();
+    normalRect = CCRectMake(0, 0, imageSize.width/2, imageSize.height);
+    selectedRect = CCRectMake(imageSize.width/2, 0, imageSize.width/2, imageSize.height);
     CCSprite* exitBtnNormal = CCSprite::create("menu_exit.png", normalRect);
     CCSprite* exitBtnSelected = CCSprite::create("menu_exit.png", selectedRect);
     CCMenuItemSprite* exitBtn = CCMenuItemSprite::create(exitBtnNormal, exitBtnSelected, this, menu_selector(MainmenuScene::clickExitBtn));
     exitBtn->setAnchorPoint(ccp(0, 1));
-    exitBtn->setPosition(Tools::getFinalPosition(ccp(870, 110)));
+    exitBtn->setPosition(Tools::getFinalPosition(ccp(POS_EXITBTN_X, POS_EXITBTN_Y)));
     
+    //all button in menu
     CCMenu* Menu = CCMenu::create(startBtn, soundBtn, exitBtn, NULL);
     Menu->setPosition(VisibleRect::leftBottom());
     this->addChild(Menu);
@@ -104,7 +106,7 @@ void MainmenuScene::clickSoundBtn(CCObject* pSender)
         CocosDenshion::SimpleAudioEngine::sharedEngine()->stopBackgroundMusic();
         Tools::m_bSoundOn = false;
     }
-    CCUserDefault::sharedUserDefault()->setBoolForKey("soundon", Tools::m_bSoundOn);
+    CCUserDefault::sharedUserDefault()->setBoolForKey(DATA_NAME_SOUNDON, Tools::m_bSoundOn);
     CCUserDefault::sharedUserDefault()->flush();
 }
 
